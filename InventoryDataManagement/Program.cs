ffe7a8ac12322dc651293b5aeab337b7889879bc
Diwakar <diwakarn1599@gmail.com>
2021-07-05 14:35:34 +0530

    [Diwakar] Initial Commit with Welcome message

---
+++ InventoryDataManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************Welcome to Inventory Management Program**************");
+        }
+    }
+}
