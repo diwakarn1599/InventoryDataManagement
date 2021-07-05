6e7c5651b12152c201179636409ca133217c5e66
Diwakar <diwakarn1599@gmail.com>
2021-07-05 18:19:47 +0530

    [Diwakar] fetch from json and displaying all inventories with total price

---
+++ InventoryDataManagement/IInventoryManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryDataManagement
+{
+    interface IInventoryManager
+    {
+        public void DisplayProducts(List<InventoryUtility.Rice> rice);
+        public void DisplayProducts(List<InventoryUtility.Wheat> Wheat);
+        public void DisplayProducts(List<InventoryUtility.Pulse> pulse);
+        public int CalculatePrice(int weight, int price);
+    }
+}
--- InventoryDataManagement/Program.cs
+++ InventoryDataManagement/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace InventoryDataManagement
 {
@@ -7,6 +12,32 @@ namespace InventoryDataManagement
         static void Main(string[] args)
         {
             Console.WriteLine("*************Welcome to Inventory Management Program**************");
+
+            InventoryManager im = new InventoryManager();
+
+            string jsonFilePath = @"C:\Users\NARD'S IDEAPAD\source\repos\InventoryDataManagement\InventoryDataManagement\Products.json";
+
+            InventoryUtility utility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(jsonFilePath));
+
+            Console.WriteLine("************MENU**********");
+            Console.WriteLine("1.Display Rice \n2.Display Pulse\n3.Display Wheat\n4.Exit\nEnter Your Option");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    im.DisplayProducts(utility.riceList);
+                    break;
+                case 2:
+                    im.DisplayProducts(utility.pulseList);
+                    break;
+                case 3:
+                    im.DisplayProducts(utility.wheatList);
+                    break;
+                case 4:
+                    Console.WriteLine("Exited");
+                    break;
+            }
+
+
         }
     }
 }
